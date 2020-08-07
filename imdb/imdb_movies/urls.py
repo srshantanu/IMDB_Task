38ed59d0e245ef260ede7c50f96da22162041f86
srshantanu <shantanupandey24@gmail.com>
2020-08-07 16:14:35 +0530

    updated app urls

--- imdb/imdb_movies/urls.py
+++ imdb/imdb_movies/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from .api.api_movies_view import SearchMovieListView
+from .api.api_movies_view import api_all_movies_list
 from .api import api_urls
 
 urlpatterns = [
-    path('',SearchMovieListView, name='movie_list'),
+    path('',api_all_movies_list, name='movie_list'),
     path('api/movie/', include(api_urls), name='movie_api')
 
     #REST API urls
